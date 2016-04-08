46703fa34e5621e9449d508851538c8a2fa9d6e8
Vulcun <gilbert@vulcun.com>
2016-04-07 22:26:55 -0700

    added twilio and jwt packages

--- app/core/View.php
+++ app/core/View.php
@@ -11,6 +11,16 @@ class View {
 		require sprintf("%s/views/%s", APP_PATH, $view);
 		return ob_get_flush();
 	}
+
+	/*
+	public function ijson() {
+		//TODO need 404 view
+		$json = file_get_contents('php://input');
+		if (!isset($json)) return $this->rjsone(400, 'Invalid Input');
+		$obj = json_decode($json, true);
+		return $obj;
+	}
+	*/
 }
 
 ?>
